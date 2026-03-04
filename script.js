04f3d546eb0463bd0e95ec28a0657de5db61a27e
SeyyedZhou <seyyedzhou@gmail.com>
2026-03-04 17:19:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const config = {
-    audioSrc: 'audio/1许狗与白宝子.mp3', // 音频文件URL（可以是相对路径或绝对URL）
+    audioSrc: '1许狗与白宝子.mp3', // 音频文件URL（可以是相对路径或绝对URL）
     title: '许狗和白宝子',
     artist: '一个讲故事的小狗'
 };
